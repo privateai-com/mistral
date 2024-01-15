3c88d8845e49f39d34e480c563ee08809cb4196d
IntegralTeam <repo@sfxdx.com>
2024-01-15 12:19:55 +0300

    Use HF pipeline with LangChain

--- main.py
+++ main.py
@@ -1,21 +1,35 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer, TextStreamer
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+from langchain.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain.prompts import PromptTemplate
 
 model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
+# This will wrap prompt into [INST], <s> and other necessary tokens 
 tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
 
-messages = [
-    {"role": "user", "content": "What is your favourite condiment?"},
-    {
-        "role": "assistant",
-        "content": "Well, I'm quite partial to a good squeeze of fresh lemon juice. It adds just the right amount of zesty flavour to whatever I'm cooking up in the kitchen!",
-    },
-    {"role": "user", "content": "Do you have mayonnaise recipes?"},
-]
 
-input = tokenizer.apply_chat_template(messages, return_tensors="pt")
-streamer = TextStreamer(tokenizer)
+pipe = pipeline(
+    "text-generation", 
+    model=model, 
+    tokenizer=tokenizer, 
+    max_new_tokens=1000, 
+    device=0 # device=-1 for CPU, device=0 for GPU
+)
+llm = HuggingFacePipeline(pipeline=pipe)
 
-generated_ids = model.generate(
-    input, streamer=streamer, max_new_tokens=1000, do_sample=True
+
+prompt = PromptTemplate.from_template(
+    """
+    Answer the question. Think step by step.
+
+    Question: {question}
+
+    """
 )
 
+chain = prompt | llm
+
+question = "How far can ducks fly?"
+
+res = chain.invoke({"question": question})
+
+print(res)
