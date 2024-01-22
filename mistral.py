90771bf02d3f90f48df91c2770e5ae1bd64bd966
IntegralTeam <repo@sfxdx.com>
2024-01-22 15:26:51 +0000

    First run in FP16 on 4 GPUs

--- mistral.py
+++ mistral.py
@@ -14,12 +14,13 @@ model_name = "mistralai/Mistral-7B-Instruct-v0.2"
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
     device_map="auto",
+    torch_dtype=torch.float16
 )
 
 tokenizer = AutoTokenizer.from_pretrained(
     model_name
 )
-pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=256)
+pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=2056)
 
 llm = HuggingFacePipeline(pipeline=pipe)
 
