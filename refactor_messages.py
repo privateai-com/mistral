2206d00c9185c76e86a4d9e9818670dc72dea0cd
IntegralTeam <repo@sfxdx.com>
2024-01-26 11:09:02 +0000

    Long instructions. Lots of examples. No comments

--- refactor_messages.py
+++ refactor_messages.py
@@ -56,12 +56,6 @@ refactor_messages = [
             Your answer: No triplets.
 
             ### Example ###
-
-            This example shows the correct way to decrease length of link and object
-            without loosing information. So "can be utilized as a method to" can be replaced with just
-            "can". All other words can be omitted. But in "decrease MDSC levels in vivo" all words are
-            important. So you should not omit any of them because otherwise, information will be lost.
-
             Context:
             {{
                 "subject": "Fas-FasL interactions",
@@ -76,16 +70,6 @@ refactor_messages = [
             }}
 
             ### Example ###
-
-            This example shows the correct way to split very long object and subject into separate triplets.
-            Object "presence of excessive growth factors, cytokines, or inflammatory molecules" is very long.
-            So you have to split it into different triplets. One for "growth factors", one for "cytokines", one for "inflammatory molecules".
-            Subject and link in all these new triplets are the same.
-
-            Subject "Presence of excessive growth factors, cytokines, or inflammatory molecules" is very long.
-            So you have to split it into different triplets. One for "growth factors", one for "cytokines", one for "inflammatory molecules".
-            Object and link in all these new triplets are the same.
-
             Context:
             {{
                 "subject": "Accumulation of MDSCs associated with tumor growth",
@@ -176,6 +160,193 @@ refactor_messages = [
                 "subject": "Fas-FasL interactions",
                 "link": "are a strategy to",
                 "object": "reduce MDSC levels in vivo"
+            }}
+
+            ### Example ###
+            Context:
+            {{
+                "subject": "Post-translational modifications",
+                "link": "affect",
+                "object": "protein folding, stability, activity, and function"
+            }}
+            Your answer:
+            {{
+                "subject": "Post-translational modifications",
+                "link": "affect",
+                "object": "protein folding"
+            }},
+            {{
+                "subject": "Post-translational modifications",
+                "link": "affect",
+                "object": "protein stability"
+            }},
+            {{
+                "subject": "Post-translational modifications",
+                "link": "affect",
+                "object": "protein activity"
+            }},
+            {{
+                "subject": "Post-translational modifications",
+                "link": "affect",
+                "object": "protein function"
+            }}
+
+            ### Example ###
+            Context:
+            {{                                                                                                                                                                                     
+                "subject": "Short polypeptides", 
+                "link": "containing fewer than 20–30 residues"
+                "object": "are typically not considered proteins"
+            }}
+            Your answer:
+            {{                                                                                                                                                                                     
+                "subject": "Short polypeptides", 
+                "link": "with less than 20-30 residues", 
+                "object": "are not proteins"
+            }}
+
+            ### Example ###
+            Context:
+            {{ 
+                "subject": "Proteins", 
+                "link": "consist of at least one long polypeptide"
+            }}
+            Your answer:
+            {{ 
+                "subject": "Proteins", 
+                "link": "consist of",
+                "subject": "at least one long polypeptide"
+            }}
+
+
+            ### Example ###
+            Context:
+            {{ 
+                "subject": "Proteins", 
+                "link": "have various functions", 
+                "object": "such as catalysis, DNA replication, response to stimuli, providing structure, and transport"
+            }}
+            Your answer:
+            {{ 
+                "subject": "Proteins", 
+                "link": "have function of", 
+                "object": "catalysis"
+            }},
+            {{ 
+                "subject": "Proteins", 
+                "link": "have function of", 
+                "object": "DNA replication"
+            }},
+            {{ 
+                "subject": "Proteins", 
+                "link": "have function of", 
+                "object": "response to stimuli"
+            }},
+            {{ 
+                "subject": "Proteins", 
+                "link": "have function of", 
+                "object": "providing structure"
+            }},
+            {{ 
+                "subject": "Proteins", 
+                "link": "have function of", 
+                "object": "transport"
+            }}
+             
+            ### Example ###
+            Context:
+            {{
+                "subject": "Generation of electric signals", 
+                "link": "was a significant innovation in the development of the nervous system"
+            }}
+            Your answer:
+            {{
+                "subject": "Generation of electric signals", 
+                "link": "was a significant innovation in",
+                "object": "development of nervous system"
+            }}
+
+            ### Example ###
+            Context:
+            {{ 
+                "subject": "Evidence from molecules", 
+                "link": "indicates that the capacity to generate electric signals emerged", 
+                "object": "during the Tonian period" 
+            }}
+            Your answer:
+            {{ 
+                "subject": "Evidence from molecules", 
+                "link": "indicates that", 
+                "object": "capacity to generate electric signals emerged" 
+            }},
+            {{ 
+                "subject": "Capacity to generate electric signals", 
+                "link": "emerged", 
+                "object": "during the Tonian period" 
+            }}
+
+            ### Example ###
+            Context:
+            {{
+                "subject": "Ability to generate electric signals", 
+                "link": "was a key innovation in the evolution of the nervous system"
+            }}
+            Your answer:
+            {{
+                "subject": "Ability to generate electric signals", 
+                "link": "was a key innovation in"
+                "object": "evolution of the nervous system"
+            }}
+
+            ### Example ###
+            Context:
+            {{
+                "subject": "Activated T cells",
+                "link": "produce",
+                "object": "FasL, which triggers apoptosis in MDSCs"
+            }}
+            Your answer:
+            {{
+                "subject": "Activated T cells",
+                "link": "produce",
+                "object": "FasL"
+            }},
+            {{
+                "subject": "FasL",
+                "link": "triggers",
+                "object": "apoptosis in MDSC's"
+            }}
+
+            ### Example ###
+            Context:
+            {{
+                "subject": "Activated T cells",
+                "link": "expressing FasL",
+                "object": "mediate apoptosis of MDSCs in vivo"
+            }}
+            Your answer:
+            {{
+                "subject": "Activated T cells",
+                "link": "mediate",
+                "object": "apoptosis of MDSCs in vivo"
+            }}
+
+            ### Example ###
+            Context:
+            {{
+                "subject": "Recent approval of several RNA drugs and COVID-19 mRNA vaccines",
+                "link": "suggests that this milestone is being realized"
+            }}
+            Your answer:
+            {{
+                "subject": "Recent approval of several RNA drugs",
+                "link": "suggests that",
+                "object": "this milestone is being realized"
+            }},
+            {{
+                "subject": "Recent approval of several COVID-19 mRNA vaccines",
+                "link": "suggests that",
+                "object": "this milestone is being realized"
             }}
 
             ### End of examples ###
--- triplet_messages.py
+++ triplet_messages.py
@@ -35,11 +35,99 @@ triplet_messages = [
             Your answer: No triplets.
 
             ### Example ###
+            Context: Bipolar disorder, previously known as manic depression, is a mental disorder characterized by 
+            periods of depression and periods of abnormally elevated mood that each last from days to weeks.
+            If the elevated mood is severe or associated with psychosis, it is called mania; if it is less severe, 
+            it is called hypomania. During mania, an individual behaves or feels abnormally energetic, happy or irritable,
+            and they often make impulsive decisions with little regard for the consequences.
+            There is usually also a reduced need for sleep during manic phases.
+            During periods of depression, the individual may experience crying and have a negative outlook on 
+            life and poor eye contact with others
+            Your answer: 
+            [
+                {{   
+                    "subject": "Bipolar disorder",
+                    "link": "is known as",
+                    "object": "manic depression"
+                }},
+                {{   
+                    "subject": "Bipolar disorder",
+                    "link": "is",
+                    "object": "mental disorder"
+                }},
+                {{   
+                    "subject": "Bipolar disorder",
+                    "link": "is characterized by",
+                    "object": "periods of depression"
+                }},
+                {{   
+                    "subject": "Bipolar disorder",
+                    "link": "is characterized by",
+                    "object": "periods of abnormally elevated mood"
+                }},
+                {{   
+                    "subject": "Periods of depression",
+                    "link": "last",
+                    "object": "from days to weeks"
+                }},
+                {{   
+                    "subject": "Periods of abnormally elevated mood",
+                    "link": "last",
+                    "object": "from days to weeks"
+                }},
+                {{   
+                    "subject": "Abnormally elevated mood",
+                    "link": "is called",
+                    "object": "mania"
+                }},
+                {{   
+                    "subject": "Less severe abnormally elevated mood",
+                    "link": "is called",
+                    "object": "hypomania"
+                }},
+                {{   
+                    "subject": "Individuals during mania period",
+                    "link": "feel",
+                    "object": "abnormally energetic, happy or irritable"
+                }},
+                {{   
+                    "subject": "Individuals during mania period",
+                    "link": "feel",
+                    "object": "happy"
+                }},
+                {{   
+                    "subject": "Individuals during mania period",
+                    "link": "feel",
+                    "object": "irritable"
+                }},
+                {{   
+                    "subject": "Individuals during mania period",
+                    "link": "often make",
+                    "object": "impulsive decisions"
+                }},
+                {{   
+                    "subject": "Individuals during mania period",
+                    "link": "often need",
+                    "object": "less sleep"
+                }},
+                {{   
+                    "subject": "Individuals during depression period",
+                    "link": "may experience",
+                    "object": "crying"
+                }},
+                {{   
+                    "subject": "Individuals during depression period",
+                    "link": "have",
+                    "object": "negative outlook on life"
+                }},
+                {{   
+                    "subject": "Individuals during depression period",
+                    "link": "avoid",
+                    "object": "eye contact"
+                }}
+            ]
 
-            In this example context contains special footnotes notations lile [1], [2]. They must be ignored.
-            It also shows a perfect example of triplets. Each part of triplet it neither long, nor short and 
-            all links contain verbs. You should try to find triplets like this in other contexts.
-
+            ### Example ###
             Context: A mitochondrion (/ˌmaɪtəˈkɒndriən/;[1] pl.: mitochondria) is an organelle found in the cells 
             of most eukaryotes, such as animals, plants and fungi. Mitochondria have a double membrane structure 
             and use aerobic respiration to generate adenosine triphosphate (ATP), which is used throughout the 
@@ -101,6 +189,57 @@ triplet_messages = [
                     "subject": "Term mitochondrion",
                     "link": "was coined in",
                     "object": "1898"
+                }}
+            ]
+
+            ### Example ###
+            Context: The most prevalent transmitter is glutamate, which is excitatory at well over 90% of the synapses in 
+            the human brain. The next most prevalent is gamma-Aminobutyric Acid, or GABA, which is inhibitory at more 
+            than 90% of the synapses that do not use glutamate. Although other transmitters are used in fewer synapses, they may 
+            be very important functionally. The great majority of psychoactive drugs exert their effects by altering the 
+            actions of some neurotransmitter systems. Addictive drugs such as cocaine and amphetamines exert their effects 
+            primarily on the dopamine system.
+            Your answer:
+            [
+                {{   
+                    "subject": "Most prevalent transmitter",
+                    "link": "is",
+                    "object": "glutamate"
+                }},
+                {{   
+                    "subject": "Glutamate",
+                    "link": "is excitatory at",
+                    "object": "over 90% of the synapses in the human brain"
+                }},
+                {{   
+                    "subject": "The next most prevalent transmitter",
+                    "link": "is",
+                    "object": "gamma-Aminobutyric Acid (GABA)"
+                }},
+                {{   
+                    "subject": "Gamma-Aminobutyric Acid ",
+                    "link": "is inhibitory at",
+                    "object": "over 90% of the synapses that do not use glutamate"
+                }},
+                {{   
+                    "subject": "Other transmitters",
+                    "link": "are used in",
+                    "object": "fewer synapses"
+                }},
+                {{   
+                    "subject": "Other transmitters",
+                    "link": are",
+                    "object": "very important functionally"
+                }},
+                {{   
+                    "subject": "Psychoactive drugs",
+                    "link": "alter",
+                    "object": "the actions of neurotransmitter systems"
+                }},
+                {{   
+                    "subject": "Addictive drugs",
+                    "link": "affect",
+                    "object": "dopamine system"
                 }}
             ]
 
