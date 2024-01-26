fc38e96d9edf03787caab72d33a5d95a18fe7642
IntegralTeam <repo@sfxdx.com>
2024-01-26 11:19:58 +0000

    Long instructions. Few examples. No comments

--- triplet_messages.py
+++ triplet_messages.py
@@ -22,112 +22,16 @@ triplet_messages = [
 
             ### Example ###
 
-            If no triplets can be found in the context, say "No triplets". Say only this and nothing else.
-
             Context: Placebo. To cook chicken. Fast Ferrari. Winter in Norway. George Dunlop.
             Your answer: No triplets.
 
             ### Example ###
 
-            If no triplets can be found in the context, say "No triplets". Say only this and nothing else.
-
             Context: Soap
             Your answer: No triplets.
 
             ### Example ###
-            Context: Bipolar disorder, previously known as manic depression, is a mental disorder characterized by 
-            periods of depression and periods of abnormally elevated mood that each last from days to weeks.
-            If the elevated mood is severe or associated with psychosis, it is called mania; if it is less severe, 
-            it is called hypomania. During mania, an individual behaves or feels abnormally energetic, happy or irritable,
-            and they often make impulsive decisions with little regard for the consequences.
-            There is usually also a reduced need for sleep during manic phases.
-            During periods of depression, the individual may experience crying and have a negative outlook on 
-            life and poor eye contact with others
-            Your answer: 
-            [
-                {{   
-                    "subject": "Bipolar disorder",
-                    "link": "is known as",
-                    "object": "manic depression"
-                }},
-                {{   
-                    "subject": "Bipolar disorder",
-                    "link": "is",
-                    "object": "mental disorder"
-                }},
-                {{   
-                    "subject": "Bipolar disorder",
-                    "link": "is characterized by",
-                    "object": "periods of depression"
-                }},
-                {{   
-                    "subject": "Bipolar disorder",
-                    "link": "is characterized by",
-                    "object": "periods of abnormally elevated mood"
-                }},
-                {{   
-                    "subject": "Periods of depression",
-                    "link": "last",
-                    "object": "from days to weeks"
-                }},
-                {{   
-                    "subject": "Periods of abnormally elevated mood",
-                    "link": "last",
-                    "object": "from days to weeks"
-                }},
-                {{   
-                    "subject": "Abnormally elevated mood",
-                    "link": "is called",
-                    "object": "mania"
-                }},
-                {{   
-                    "subject": "Less severe abnormally elevated mood",
-                    "link": "is called",
-                    "object": "hypomania"
-                }},
-                {{   
-                    "subject": "Individuals during mania period",
-                    "link": "feel",
-                    "object": "abnormally energetic, happy or irritable"
-                }},
-                {{   
-                    "subject": "Individuals during mania period",
-                    "link": "feel",
-                    "object": "happy"
-                }},
-                {{   
-                    "subject": "Individuals during mania period",
-                    "link": "feel",
-                    "object": "irritable"
-                }},
-                {{   
-                    "subject": "Individuals during mania period",
-                    "link": "often make",
-                    "object": "impulsive decisions"
-                }},
-                {{   
-                    "subject": "Individuals during mania period",
-                    "link": "often need",
-                    "object": "less sleep"
-                }},
-                {{   
-                    "subject": "Individuals during depression period",
-                    "link": "may experience",
-                    "object": "crying"
-                }},
-                {{   
-                    "subject": "Individuals during depression period",
-                    "link": "have",
-                    "object": "negative outlook on life"
-                }},
-                {{   
-                    "subject": "Individuals during depression period",
-                    "link": "avoid",
-                    "object": "eye contact"
-                }}
-            ]
 
-            ### Example ###
             Context: A mitochondrion (/ˌmaɪtəˈkɒndriən/;[1] pl.: mitochondria) is an organelle found in the cells 
             of most eukaryotes, such as animals, plants and fungi. Mitochondria have a double membrane structure 
             and use aerobic respiration to generate adenosine triphosphate (ATP), which is used throughout the 
@@ -189,57 +93,6 @@ triplet_messages = [
                     "subject": "Term mitochondrion",
                     "link": "was coined in",
                     "object": "1898"
-                }}
-            ]
-
-            ### Example ###
-            Context: The most prevalent transmitter is glutamate, which is excitatory at well over 90% of the synapses in 
-            the human brain. The next most prevalent is gamma-Aminobutyric Acid, or GABA, which is inhibitory at more 
-            than 90% of the synapses that do not use glutamate. Although other transmitters are used in fewer synapses, they may 
-            be very important functionally. The great majority of psychoactive drugs exert their effects by altering the 
-            actions of some neurotransmitter systems. Addictive drugs such as cocaine and amphetamines exert their effects 
-            primarily on the dopamine system.
-            Your answer:
-            [
-                {{   
-                    "subject": "Most prevalent transmitter",
-                    "link": "is",
-                    "object": "glutamate"
-                }},
-                {{   
-                    "subject": "Glutamate",
-                    "link": "is excitatory at",
-                    "object": "over 90% of the synapses in the human brain"
-                }},
-                {{   
-                    "subject": "The next most prevalent transmitter",
-                    "link": "is",
-                    "object": "gamma-Aminobutyric Acid (GABA)"
-                }},
-                {{   
-                    "subject": "Gamma-Aminobutyric Acid ",
-                    "link": "is inhibitory at",
-                    "object": "over 90% of the synapses that do not use glutamate"
-                }},
-                {{   
-                    "subject": "Other transmitters",
-                    "link": "are used in",
-                    "object": "fewer synapses"
-                }},
-                {{   
-                    "subject": "Other transmitters",
-                    "link": are",
-                    "object": "very important functionally"
-                }},
-                {{   
-                    "subject": "Psychoactive drugs",
-                    "link": "alter",
-                    "object": "the actions of neurotransmitter systems"
-                }},
-                {{   
-                    "subject": "Addictive drugs",
-                    "link": "affect",
-                    "object": "dopamine system"
                 }}
             ]
 
